f7326cb2d126f9054a4ef9c1f6040254547032f5
Saketh Ram Pusuluru <saketh.pusuluru@sasi.ac.in>
2025-07-04 14:56:45 +0530

    Ficed logo

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,39 @@ import ProfileCard from "./components/ProfileCard.jsx";
 function App() {
   return (
     <>
-      <h1
+      {/* Side-by-side header */}
+      <div
         style={{
-          textAlign: "center",
-          fontSize: "50px",
-          marginBottom: "12px",
-          color: "#333",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "20px",
+          marginTop: "20px",
+          marginBottom: "30px",
         }}
       >
-        Gradious Students
-      </h1>
+        <img
+          src="/gradious-technologies.webp"
+          alt="Gradious Logo"
+          style={{
+            width: "80px",
+            height: "80px",
+            borderRadius: "50%",
+            objectFit: "cover",
+          }}
+        />
+        <h1
+          style={{
+            fontSize: "40px",
+            color: "#333",
+            margin: 0,
+          }}
+        >
+          Gradious Students
+        </h1>
+      </div>
+
+      {/* Profile Cards Grid */}
       <div
         style={{
           display: "flex",
@@ -26,12 +49,11 @@ function App() {
         <ProfileCard
           name="Saketh Ram"
           imageUrl="/sakethcard.jpg"
-          skills={["React", "Node.js", "MongoDB","Express","MERN"]}
+          skills={["React", "Node.js", "MongoDB", "Express", "MERN"]}
           lastSeen={null}
           github="https://github.com/saketh-cell"
           viewCv="https://drive.google.com/file/d/1juKDAZBLxqmmF5QnArUfudW6chFj4yT0/view?usp=drive_link"
         />
-
         <ProfileCard
           name="Rohith"
           imageUrl="/Rohith.jpg"
@@ -44,7 +66,7 @@ function App() {
           name="Nikshith"
           imageUrl="Panthul.jpg"
           skills={["HTML", "CSS", "Javascript", "Java", "MERN"]}
-          lastSeen={null} 
+          lastSeen={null}
           github="https://github.com/DNikshith"
           viewCv="https://drive.google.com/file/d/1juKDAZBLxqmmF5QnArUfudW6chFj4yT0/view?usp=drive_link"
         />
@@ -60,7 +82,7 @@ function App() {
           name="Hemanth"
           imageUrl="/Hemanth.jpg"
           skills={["MongoDB", "MySQL", "NoSQL", "MERN"]}
-          lastSeen={null} // Online
+          lastSeen={null}
           github="https://github.com/Atlahemanthkumarreddy"
           viewCv="https://drive.google.com/file/d/1juKDAZBLxqmmF5QnArUfudW6chFj4yT0/view?usp=drive_link"
         />
@@ -68,17 +90,15 @@ function App() {
           name="Jaswanth"
           imageUrl="/Jaswanth.jpg"
           skills={["BPO", "HR", "Product Management", "Javascript"]}
-          lastSeen={new Date(
-            Date.now() - 1000 * 60 * 60 * 24 * 10
-          ).toISOString()}
+          lastSeen={new Date(Date.now() - 1000 * 60 * 60 * 24 * 10).toISOString()}
           github="https://github.com/EnagantiRohith"
           viewCv="https://drive.google.com/file/d/1juKDAZBLxqmmF5QnArUfudW6chFj4yT0/view?usp=drive_link"
         />
         <ProfileCard
           name="Chari"
           imageUrl="/Chari.jpg"
-          skills={["Python", "Java", "MERN", "JavaScript","Flask"]}
-          lastSeen={null} // Online
+          skills={["Python", "Java", "MERN", "JavaScript", "Flask"]}
+          lastSeen={null}
           github="https://github.com/Venkateshchary07"
           viewCv="https://drive.google.com/file/d/1juKDAZBLxqmmF5QnArUfudW6chFj4yT0/view?usp=drive_link"
         />
@@ -94,7 +114,7 @@ function App() {
           name="Aravind"
           imageUrl="/Aravind.jpg"
           skills={["Javascript", "Java", "React", "MERN"]}
-          lastSeen={null} // Online
+          lastSeen={null}
           github="https://github.com/EnagantiRohith"
           viewCv="https://drive.google.com/file/d/1juKDAZBLxqmmF5QnArUfudW6chFj4yT0/view?usp=drive_link"
         />
